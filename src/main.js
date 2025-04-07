8ad76cefff178e26eeb6ffd074d419cfbed209db
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-07 16:24:04 +0900

    chore: tailwind 설정

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
+import { createPinia } from "pinia";
+import { createApp } from "vue";
 
-import App from './App.vue';
-import router from './router';
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App.vue";
+import "./assets/tailwind.css";
+import router from "./router";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
