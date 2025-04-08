154f019402d7de53dd4281ac0274affd2d7d725f
junbeom <jach1206@naver.com>
2025-04-08 11:47:53 +0900

    feat: 메인 페이지 수입 및 지출 계산 기능 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+<<<<<<< Updated upstream
       path: '/',
       name: 'detail',
       component: () => import('../pages/detailPage.vue'),
     }
+=======
+      path: '/main',
+      name: 'main',
+      component: () => import('../pages/MainPage.vue'),
+    },
+>>>>>>> Stashed changes
   ],
 });
 
