2cd0b07355b58906ba027529cfa759ac498c8d48
junbeom <jach1206@naver.com>
2025-04-08 17:03:51 +0900

    feat: report page 내부 기능구현

--- src/pages/ReportPage.vue
+++ src/pages/ReportPage.vue
@@ -14,12 +14,16 @@
             type="date"
             class="form-control form-control-sm"
             style="width: 120px"
+            v-model="startDate"
+            @change="toInteger"
           />
           <span class="mx-1">-</span>
           <input
             type="date"
             class="form-control form-control-sm"
             style="width: 120px"
+            v-model="endDate"
+            @change="toInteger"
           />
         </div>
       </div>
@@ -57,17 +61,23 @@
     <div class="mb-4 d-flex justify-content-end">
       <button
         class="btn btn-sm text-black px-3 border border-gray-400 rounded-start-pill"
-        :class="selected === 'income' ? 'bg-custom-purple' : 'bg-white'"
-        @click="selected = 'income'"
+        :class="selected ? 'bg-custom-purple' : 'bg-white'"
+        @click="selected = true"
       >
         수입
       </button>
       <button
         class="btn btn-sm text-black px-3 border border-gray-400 border-start-0 rounded-end-pill"
-        :class="selected === 'expense' ? 'bg-custom-purple' : 'bg-white'"
-        @click="selected = 'expense'"
+        :class="!selected ? 'bg-custom-purple' : 'bg-white'"
+        @click="selected = false"
       >
         지출
+      </button>
+      <button
+        class="btn btn-sm border rounded-pill px-3 flex-shrink-0"
+        @click="getData"
+      >
+        조회
       </button>
     </div>
 
@@ -75,66 +85,97 @@
     <div
       class="mb-4 border rounded px-3 py-2 d-flex justify-content-between small"
     >
-      <span>15건</span>
-      <span class="fw-bold">+2,142,200</span>
+      <span>{{ allData.length }}건</span>
+      <span class="fw-bold">{{ sumAmount.toLocaleString() }}</span>
     </div>
 
     <!-- 거래 내역 카드 -->
     <div class="d-flex flex-column gap-2">
       <TransactionCard
-        category="식비"
-        title="일품순대국"
-        date="2025년 4월 30일"
-        :amount="15300"
-        :isIncome="false"
-      />
-      <TransactionCard
-        category="월급"
-        title="무한상사"
-        date="2025년 5월 01일"
-        :amount="2300300"
-        :isIncome="true"
-      />
-      <TransactionCard
-        category="교통비"
-        title="교통비"
-        date="2025년 5월 01일"
-        :amount="75900"
-        :isIncome="false"
+        v-for="data in allData"
+        :key="data.id"
+        :category="data.category"
+        :title="data.name"
+        :date="data.date"
+        :amount="data.amount"
+        :isIncome="data.type"
       />
     </div>
   </div>
 </template>
 
 <script setup>
-import TransactionCard from "@/components/Report/TransactionCard.vue";
-import BackButton from "@/components/Shared/BackButton.vue";
-import Header from "@/components/Shared/Header.vue";
-import { ref, watch } from "vue";
-import { useRouter } from "vue-router";
+import TransactionCard from '@/components/Report/TransactionCard.vue';
+import BackButton from '@/components/Shared/BackButton.vue';
+import Header from '@/components/Shared/Header.vue';
+import axios from 'axios';
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 
 const router = useRouter();
 const goBack = () => {
-  router.back();
+  router.push('/main');
 };
 
-const selected = ref("income");
-const selectedCategory = ref("전체");
+const selected = ref(true);
+const selectedCategory = ref('전체');
+const startDate = ref('2025-01-01');
+const endDate = ref('2025-04-08');
+const selectedStartDate = ref(1);
+const selectedEndDate = ref(999999999);
+const sumAmount = ref(0);
 
-const categories = [
-  "식비",
-  "교통비",
-  "월급",
-  "기타1",
-  "기타2",
-  "기타3",
-  "식비2",
-  "교통비2",
-  "월급2",
-];
+localStorage.setItem('id', 1);
+const userId = localStorage.getItem('id');
+
+const categories = ['공과금', '경조사비', '식비', '월급', '여가비', '기타'];
+
+const allData = ref([
+  {
+    amount: 0,
+    category: '',
+    date: 0,
+    memo: '',
+    name: '',
+    type: true,
+  },
+]);
+
+function calTotalSum() {
+  sumAmount.value = 0;
+  allData.value.forEach((e) => {
+    sumAmount.value += e.amount;
+  });
+}
+
+function toInteger() {
+  selectedStartDate.value = Number(startDate.value.replaceAll('-', ''));
+  selectedEndDate.value = Number(endDate.value.replaceAll('-', ''));
+}
+
+async function getData() {
+  let res = '';
+  if (selectedCategory.value === '전체') {
+    res = await axios.get(
+      `http://localhost:3000/transaction?memberId=${userId}&date_gte=${selectedStartDate.value}&date_lte=${selectedEndDate.value}&type=${selected.value}`
+    );
+  } else {
+    res = await axios.get(
+      `http://localhost:3000/transaction?memberId=${userId}&date_gte=${selectedStartDate.value}&date_lte=${selectedEndDate.value}&category=${selectedCategory.value}&type=${selected.value}`
+    );
+  }
+  allData.value = res.data;
+  calTotalSum();
+  selected.value
+    ? (sumAmount.value = '+' + sumAmount.value.toLocaleString())
+    : '-' + sumAmount.value.toLocaleString();
+  console.log(allData.value);
+}
+
+getData();
 
 watch(selectedCategory, (newVal) => {
-  console.log("선택된 카테고리:", newVal);
+  console.log('선택된 카테고리:', newVal);
 });
 </script>
 
