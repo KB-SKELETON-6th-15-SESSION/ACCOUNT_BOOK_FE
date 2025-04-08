e216c986ff182b00bddcb28750afc1a5dca9ae0e
KIM HUIJU <gmlwnchlrh@gmail.com>
2025-04-08 15:30:25 +0900

    feat: 프로필 수정 페이지 axios 데이터 연동

--- src/pages/ModifyPage.vue
+++ src/pages/ModifyPage.vue
@@ -11,33 +11,62 @@
       <input type="text" v-model="member.name" />
 
       <label>이메일</label>
-      <input v-model="member.email" disabled />
+      <input type="email" v-model="member.email" disabled />
 
       <label>비밀번호</label>
       <input type="password" v-model="member.password" />
     </div>
 
-    <MainButton>프로필 수정하기</MainButton>
+    <MainButton @click="updateMember">프로필 수정하기</MainButton>
   </div>
 </template>
 
 <script setup>
-import BackButton from '@/components/Shared/BackButton.vue';
 import Header from '@/components/Shared/Header.vue';
+import BackButton from '@/components/Shared/BackButton.vue';
 import MainButton from '@/components/Shared/MainButton.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
+import axios from 'axios';
 
 const router = useRouter();
 
 const member = ref({
-  name: '강민재',
-  email: 'minijae011030@gmail.com',
+  name: '',
+  email: '',
   password: '',
+});
+
+const id = localStorage.getItem('id');
+
+onMounted(async () => {
+  //   if (!id) {
+  //     alert('로그인이 필요합니다');
+  //     router.push('/login');
+  //     return;
+  //   }
+
+  const res = await axios.get(`http://localhost:3000/member/${id}`);
+  member.value = {
+    name: res.data.name,
+    email: res.data.email,
+    password: res.data.password,
+  };
 });
 
 function goBack() {
   router.push('/profile');
+}
+
+async function updateMember() {
+  try {
+    await axios.put(`http://localhost:3000/member/${id}`, member.value);
+    alert('수정 완료');
+    router.push('/profile');
+  } catch (error) {
+    console.error(error);
+    alert('수정 실패');
+  }
 }
 </script>
 
