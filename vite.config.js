9714946d9e994412323100453bcb457f24dd67df
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-10 10:42:36 +0900

    feat: pwa

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    VitePWA({
+      registerType: "autoUpdate", // 서비스워커 자동 업데이트
+      manifest: {
+        name: "텅장지킴이",
+        short_name: "텅장",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#B54CE9",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
